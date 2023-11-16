2e83ac52948860692af3c7887e31a9587241fd92
Zhanghaoismine <kegberry@naver.com>
2023-11-16 16:07:33 +0900

    5

--- main.c
+++ main.c
@@ -2,24 +2,20 @@
 #include <stdlib.h>
 
 int main(void){
-	int i;
-	int grade[5];
-	int average=0;
+	char *pc;
+	int *pi;
+	double *pd;
 	
-	for(i=0;i<5;i++)
-	{
-		printf("input value[%i]=",i);
-		scanf("%d",&grade[i]);
-	}
-	//반복문 및 포인터 문법을 통해 grade 배열\
-	//값 출력 및 평균 계산
-		for(i=0;i<5;i++)
-	{
-		printf("grade[%i]=%i\n",*(grade+i));
-		average=(average+*(grade+i));
-	}
-		printf("average=%i\n", average/5);
-		system("PAUSE");
+	pc=(char*)10000;
+	pi=(int*)10000;
+	pd=(double*)10000;
+	
+	printf("증가 전: pc=%d, pi=%d, pd=%d\n",pc,pi,pd);
+	
+	pc++;
+	pi++;
+	pd++;
+	printf("증가 후:pc=%d, pi=%d, pd=%d\n",pc,pi,pd);
 	
 	return 0;
 }
