c378dd8f8a01a2dcc7c7a40e9876047e23241be1
Zhanghaoismine <kegberry@naver.com>
2023-11-16 15:58:01 +0900

    4

--- main.c
+++ main.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap(int *x, int *y){
-	int temp=*x;
-	*x=*y;
-	*y=temp;
-}
-void main(void) {
-	int a=3;
-	int b=5;
-	swap(&a,&b);
-	printf("a: %i, b: %i\n", a, b);
+int main(void){
+	int i;
+	int grade[5];
+	int average=0;
+	
+	for(i=0;i<5;i++)
+	{
+		printf("input value[%i]=",i);
+		scanf("%d",&grade[i]);
+	}
+	//반복문 및 포인터 문법을 통해 grade 배열\
+	//값 출력 및 평균 계산
+		for(i=0;i<5;i++)
+	{
+		printf("grade[%i]=%i\n",*(grade+i));
+		average=(average+*(grade+i));
+	}
+		printf("average=%i\n", average/5);
+		system("PAUSE");
+	
+	return 0;
 }
